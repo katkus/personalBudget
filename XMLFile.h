67f78db3f19677120ccaeef1ccc3e033982435d8
Katarzyna Ziemba <katarzyna.kuswik.programista@gmail.com>
2024-05-14 21:36:08 +0200

    Added base class XMLFile to file

---
+++ XMLFile.h
@@ -0,0 +1,30 @@
+#ifndef XMLFILE_H
+#define XMLFILE_H
+
+#include <iostream>
+#include <fstream>
+
+#include "Markup.h"
+
+using namespace std;
+
+class XMLFile {
+private:
+    const string FILE_NAME;
+
+protected:
+    int lastId;
+    CMarkup xml;
+
+    string getFileName();
+    static bool isFileEmpty(fstream &XMLFile);
+
+    XMLFile(string fileName): FILE_NAME (fileName) {
+    lastId=0;
+    };
+
+public:
+    int getLastId();
+};
+#endif // XMLFILE_H
+
